52febaa9199265f77460a98a2945eae6f0b7dd2c
anton-1108 <antonamarjargal27@gmail.com>
2025-02-20 22:39:29 +0800

    three

--- src/app/_components/CloudinaryUpload.tsx
+++ src/app/_components/CloudinaryUpload.tsx
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 
 export const CloudinaryUpload = () => {
   const [file, setFile] = useState(null);
-  const [image, setimage] = useState(null);
+  const [image, setImage] = useState(null);
 
   const PRESENT_NAME = "food-delivery-app";
-  const CLODINARY_NAME = "dnpwi1bxt";
+  const CLODINARY_NAME = "dquipdgj5";
 
-  const handleFile = (e) => {
+  const handleFile = (e: any) => {
     const file = e.target.files[0];
     if (file) {
       setFile(file);
@@ -24,7 +24,22 @@ export const CloudinaryUpload = () => {
     formData.append("file", file);
     formData.append("upload_present", PRESENT_NAME);
     formData.append("api_key", CLODINARY_NAME);
-
-    const res = await fetch("");
+    try {
+      const res = await fetch(
+        `https://api.cloudinary.com/v1_1/${CLODINARY_NAME}/image/upload`,
+        {
+          method: "POST",
+          body: "formData",
+        }
+      );
+      const data = await res.json();
+      setImage(data.secure_url);
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+      alert("Failed to upload file");
+    }
   };
+  console.log(file);
+  return <div></div>;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Header from "@/app/_components/Header";
+import Footer from "./_components/Footer";
 export default function Home() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
