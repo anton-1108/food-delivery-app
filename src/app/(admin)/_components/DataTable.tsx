07c0964b2e4c6c211d4e6e81b4994447fe58081c
anton-1108 <antonamarjargal27@gmail.com>
2025-03-10 10:42:06 +0800

    kkkkk

--- src/app/(admin)/_components/DataTable.tsx
+++ src/app/(admin)/_components/DataTable.tsx
@@ -42,31 +42,31 @@ const data: Payment[] = [
     id: "m5gr84i9",
     amount: 316,
     status: "success",
-    email: "ken99@yahoo.com",
+    email: "Test@yahoo.com",
   },
   {
     id: "3u1reuv4",
     amount: 242,
     status: "success",
-    email: "Abe45@gmail.com",
+    email: "Teest@gmail.com",
   },
   {
     id: "derv1ws0",
     amount: 837,
     status: "processing",
-    email: "Monserrat44@gmail.com",
+    email: "Test@gmail.com",
   },
   {
     id: "5kma53ae",
     amount: 874,
     status: "success",
-    email: "Silas22@gmail.com",
+    email: "Test@gmail.com",
   },
   {
     id: "bhqecj4p",
     amount: 721,
     status: "failed",
-    email: "carmella@hotmail.com",
+    email: "Test@hotmail.com",
   },
 ];
 
