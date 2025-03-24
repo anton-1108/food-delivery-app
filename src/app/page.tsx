fa8bad1c5da0bc994340b6984202caec2a6a8082
anton-1108 <antonamarjargal27@gmail.com>
2025-03-24 09:13:11 +0800

    jjj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Header from "@/app/_components/Header";
 import Footer from "./_components/Footer";
 import MenuCloud from "./_components/MenuCloud";
+import Card2 from "./_components/Foodcard";
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#404040]">
       <Header />
       <MenuCloud />
+      <Card2 />
       <Footer />
     </div>
   );
